4868ca613256c5dfafc303c2378433816f354681
dangminhngo <dangminhngo.dev@gmail.com>
2024-01-28 10:14:33 +0700

    fix: fix nvim ui

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -1,4 +1,113 @@
+local Util = require "lazyvim.util"
+
 return {
+  -- file explorer
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    cmd = "Neotree",
+    keys = {
+      {
+        "<leader>fe",
+        function()
+          require("neo-tree.command").execute { toggle = true, dir = Util.root() }
+        end,
+        desc = "Explorer (root)",
+      },
+      {
+        "<leader>fE",
+        function()
+          require("neo-tree.command").execute { toggle = true, dir = vim.loop.cwd() }
+        end,
+        desc = "Explorer (cwd)",
+      },
+      { "<leader>e", "<leader>fe", desc = "Explorer (root)", remap = true },
+      { "<leader>E", "<leader>fE", desc = "Explorer (cwd)", remap = true },
+    },
+    deactivate = function()
+      vim.cmd [[Neotree close]]
+    end,
+    init = function()
+      vim.g.neo_tree_remove_legacy_commands = 1
+      if vim.fn.argc() == 1 then
+        local stat = vim.loop.fs_stat(vim.fn.argv(0))
+        if stat and stat.type == "directory" then
+          require "neo-tree"
+        end
+      end
+    end,
+    opts = {
+      sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+      open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline" },
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = {
+          enabled = true,
+        },
+        use_libuv_file_watcher = true,
+      },
+      window = {
+        width = 36,
+        mappings = {
+          ["<space>"] = "none",
+          ["o"] = "open",
+        },
+      },
+      default_component_configs = {
+        indent = {
+          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+          expander_collapsed = "󰅂",
+          expander_expanded = "󰅀",
+          expander_highlight = "NeoTreeExpander",
+        },
+        icon = {
+          folder_closed = "󰉋",
+          folder_open = "󰝰",
+          folder_empty = "󰷏",
+          default = "󰈙",
+        },
+        git_status = {
+          symbols = {
+            -- Change type
+            added = "󰐙", -- or "✚", but this is redundant info if you use git_status_colors on the name
+            modified = "󰆗", -- or "", but this is redundant info if you use git_status_colors on the name
+            deleted = "󰍷", -- this can only be used in the git_status source
+            renamed = "󰝶", -- this can only be used in the git_status source
+            -- Status type
+            untracked = "󰛑",
+            ignored = "󱃓",
+            unstaged = "󰝦",
+            staged = "󰄳",
+            conflict = "",
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      local function on_move(data)
+        Util.lsp.on_rename(data.source, data.destination)
+      end
+
+      local events = require "neo-tree.events"
+      opts.event_handlers = opts.event_handlers or {}
+      vim.list_extend(opts.event_handlers, {
+        { event = events.FILE_MOVED, handler = on_move },
+        { event = events.FILE_RENAMED, handler = on_move },
+      })
+
+      require("neo-tree").setup(opts)
+      vim.api.nvim_create_autocmd("TermClose", {
+        pattern = "*lazygit",
+        callback = function()
+          if package.loaded["neo-tree.sources.git_status"] then
+            require("neo-tree.sources.git_status").refresh()
+          end
+        end,
+      })
+    end,
+  },
+
+  -- flash.nvim
   {
     enabled = false,
     "folke/flash.nvim",
--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -1,3 +1,6 @@
+local icons = require "config.icons"
+local Util = require "lazyvim.util"
+
 return {
   {
     "rcarriga/nvim-notify",
@@ -29,9 +32,6 @@ return {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     opts = function()
-      local icons = require "config.icons"
-      local Util = require "lazyvim.util"
-
       return {
         options = {
           theme = "auto",
@@ -140,6 +140,91 @@ return {
 
       logo = string.rep("\n", 8) .. logo .. "\n\n"
       opts.config.header = vim.split(logo, "\n")
+
+      opts.config.center = {
+        {
+          action = "Telescope find_files",
+          desc = " Find file",
+          icon = "󰈞 ",
+          key = "f",
+        },
+        {
+          action = "ene | startinsert",
+          desc = " New file",
+          icon = "󰈔 ",
+          key = "n",
+        },
+        {
+          action = "Telescope oldfiles",
+          desc = " Recent files",
+          icon = "󰷊 ",
+          key = "r",
+        },
+        {
+          action = "Telescope live_grep",
+          desc = " Find text",
+          icon = "󰘎 ",
+          key = "g",
+        },
+        {
+          action = [[lua require("lazyvim.util").telescope.config_files()()]],
+          desc = " Config",
+          icon = "󰒓 ",
+          key = "c",
+        },
+        {
+          action = 'lua require("persistence").load()',
+          desc = " Restore Session",
+          icon = "󰦛 ",
+          key = "s",
+        },
+        {
+          action = "LazyExtras",
+          desc = " Lazy Extras",
+          icon = " ",
+          key = "x",
+        },
+        {
+          action = "Lazy",
+          desc = " Lazy",
+          icon = "󰒲 ",
+          key = "l",
+        },
+        {
+          action = "qa",
+          desc = " Quit",
+          icon = "󰿅 ",
+          key = "q",
+        },
+      }
+
+      for _, button in ipairs(opts.config.center) do
+        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+        button.key_format = "  %s"
+      end
+
+      -- close Lazy and re-open when the dashboard is ready
+      if vim.o.filetype == "lazy" then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "DashboardLoaded",
+          callback = function()
+            require("lazy").show()
+          end,
+        })
+      end
+
+      return opts
     end,
+  },
+
+  -- web devicons
+  {
+    "nvim-tree/nvim-web-devicons",
+    lazy = true,
+    opts = {
+      override_by_filename = icons.devicons.filename,
+      override_by_extension = icons.devicons.extension,
+    },
   },
 }
